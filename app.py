65b7eb933eb4061ea4acaa99a6a7a7d6c14d931e
piewq <145069080+piewq@users.noreply.github.com>
2025-01-07 22:29:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,47 @@ import pandas as pd
 print(pd.__version__)
 import plotly.express as px
 import os
+import requests 
+from datetime import datetime
+
+def get_books(query):
+    url=f'https://www.googleapis.com/books/v1/volumes?q={query}&maxResults=10'
+    response = requests.get(url)
+    books = response.json().get('items', [])
+    return books
+st.title("Библиотека книг")
+
+#Ввод запроса для поиска
+query = st.text_input("Поиск книги", "Harry Potter")
+
+#Find authors and genres
+authors = set()
+genres = set()
+
+if query:
+    books = get_books(query)
+    if books:
+        fir book in books:
+        author_list = book['volumeInfo'].get('authors', [])
+        genre_list = book['volumeInfo'].get('categories', [])
+        authors.update(author_list)
+        genres.update(genre_list)
+
+#Выводим авторов и жанры в виде выпадающего списка
+selected_author = st.selectbox("Выберите автора", list(authors))
+selected_genre = st.selectbox("Выберите жанр", list(genres))
+
+#Показать информацию о книгах
+for book in books:
+    title = book['volumeInfo'].get('title', 'Неизвестно')
+    authors = ', '.join(book['volumeInfo'].get('authors', ['Неизвестно']))
+    description = book['volumeInfo'].get('description', 'Нет описания')
+    st.write(f"Название: {title}")
+    st.write(f"Авторы: {authors}")
+    st.write(f"Описание: {description}")
+    st.write("---")
+    else:
+        st.write("Книги не найдены.")
 
 #File with data
 data_file = "books_data.csv"
@@ -32,8 +73,14 @@ with st.form("book_form"):
     start_date = st.date_input("Дата начала")
     end_date = st.date_input("Дата конца")
 
+    if end_date < start_date:
+        st.error("Неа")
+    else:
+        st.success("Ok")
+
 #Оценка книги
-    rating = st.slider("Оценка", 1, 5, 3)
+    rating = st.slider("Оценка",min_value=1.0, max_value=5.0, step=0.5)
+    st.write(f"Вы выбрали оценку: {rating}")
 
     submit = st.form_submit_button("Добавить")
 
